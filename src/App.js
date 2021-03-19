af41fb10568e58f88d0f3bdf65fbb57b2e116d63
Justin Peczenij <justinpeczenij@gmail.com>
2021-03-19 17:31:02 -0400

    created components and styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import styled from 'styled-components'
+
+import axios from 'axios';
+import Character from './components/Character'
+// import { set } from 'msw/lib/types/context';
+
+const StyledH1 = styled.h1 `
+  font-size: 4rem;
+  color: lime;
+  font-family: 'Chango', cursive;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [ characters, setCharacters] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people/?page=1')
+      .then(res => {
+        console.log(res.data)
+        setCharacters(res.data)
+      })
+      .catch(err => {
+        debugger;
+      })
+    }, [])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className="App" style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <StyledH1 className="Header">REACT WARS/></StyledH1>
+        {
+          characters.map(character => {
+            return <Character key={character.name} details={character} />
+          })
+        }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,41 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    width: 40%;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    font-family: 'Syne Mono', monospace;
+    border: 1px solid #4AFD34;
+    margin: 5px 0;
+    padding: 0 1%;
+`;
+
+const StyledH2 = styled.h2`
+    font-size: 2rem;
+    color: #f4f4f4;
+    text-shadow: 0.2rem 0.2rem black;
+`;
+
+const StyledH3 = styled.h2`
+    font-size: 1.5rem;
+    color: #f4f4f4;
+    text-shadow: 0.2rem 0.2rem black;
+`;
+
+// const WideDiv = styled.div`
+//     width: 50%;
+//     border:1px solid red;
+// `;
+
+
+export default function Character({ details }) {
+    return(
+        <StyledDiv>
+            <StyledH2>{details.name}</StyledH2>
+            <StyledH3>Weight: {Math.round(details.mass * 2.2)} pounds</StyledH3>
+        </StyledDiv>
+    )
+}
